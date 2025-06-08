25e31a9d9dcba4667322696cff95fb4eb0427187
julianMaranta <julianmarantaguasch@gmail.com>
2025-06-07 23:42:21 -0300

    entry route

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,11 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   
-  {
-    path: '/entry-option',
-    name: 'entry-option',
-    component: () => import('../views/EntryOptionView.vue')
-  },
+
   {
     path: '/',
     name: 'inicio',
